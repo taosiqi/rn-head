1c20d9073fb1eb2cdb53a3d0b76b1e4d00774812
陶思淇 <thinklf97@gmail.com>
2022-12-04 15:52:12 +0800

    init:Navigator

--- src/App16.tsx
+++ src/App16.tsx
@@ -1,15 +1,54 @@
-import * as React from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {FC} from 'react';
 import RNBootSplash from 'react-native-bootsplash';
+import {
+  NavigationContainer,
+  NavigationProp,
+  RouteProp,
+} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+interface Props {
+  navigation: NavigationProp<any>;
+  route: RouteProp<any>;
+}
+const HomeScreen: FC<Props> = props => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          props.navigation.navigate('detail');
+        }}
+      />
+    </View>
+  );
+};
+function DetailsScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   setTimeout(() => {
     RNBootSplash.hide({fade: true});
   }, 2000);
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>123</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="home"
+          options={{title: 'Awesome app'}}
+          component={HomeScreen}
+        />
+        <Stack.Screen name="detail" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
